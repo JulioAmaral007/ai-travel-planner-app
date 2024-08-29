5b04c73d44a6fd379694eab0a2a08cccac059cb6
Júlio Cézar <jczrpereira77@gmail.com>
2024-08-29 19:47:47 -0300

    feat: Tidied up the onboarding screen

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,17 +1,18 @@
+import TripProvider from '@/context/TripContext'
 import {
   Nunito_400Regular,
   Nunito_500Medium,
   Nunito_600SemiBold,
   Nunito_700Bold,
-  useFonts
-} from '@expo-google-fonts/nunito';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import "../styles/global.css";
+  useFonts,
+} from '@expo-google-fonts/nunito'
+import { Stack } from 'expo-router'
+import * as SplashScreen from 'expo-splash-screen'
+import { StatusBar } from 'expo-status-bar'
+import { useEffect } from 'react'
+import '../styles/global.css'
 
-SplashScreen.preventAutoHideAsync();
+SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
   const [loaded] = useFonts({
@@ -19,27 +20,31 @@ export default function RootLayout() {
     Nunito_500Medium,
     Nunito_600SemiBold,
     Nunito_700Bold,
-  });
+  })
 
   useEffect(() => {
     if (loaded) {
-      SplashScreen.hideAsync();
+      SplashScreen.hideAsync()
     }
-  }, [loaded]);
+  }, [loaded])
 
   if (!loaded) {
-    return null;
+    return null
   }
 
   return (
     <>
-      <StatusBar style="inverted" />
-      
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+      <StatusBar style="dark" />
+
+      <TripProvider>
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="create-trip" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+      </TripProvider>
     </>
-  );
+  )
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,20 +1,23 @@
-import { router } from "expo-router";
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import { router } from 'expo-router'
+import { Image, Text, TouchableOpacity, View } from 'react-native'
 
 export default function WelcomeScreen() {
   return (
-    <View className="flex-1 bg-[#181B26]">
-      <Image source={require("../assets/onboarding.jpg")} className="w-full h-full absolute" />
+    <View className="flex-1 bg-white">
+      <Image source={require('../assets/onboarding.jpg')} className="w-full h-full absolute" />
       <View className="flex-1 items-center">
-        <Image source={require("../assets/logo.png")} className="w-[242px] h-[132px] mt-32" />
+        <Image source={require('../assets/logo.png')} className="w-[242px] h-[132px] mt-32" />
       </View>
 
       <View className="bg-white/70 h-80 rounded-t-[30px] p-5 justify-center items-center">
-        <View className="bg-orange-600/70 rounded-3xl w-56 h-2 absolute top-3" />
+        <View className="bg-green-50/70 rounded-3xl w-56 h-2 absolute top-3" />
         <Text className="font-semibold text-7xl pt-5">TripGenius</Text>
         <Text className="text-3xl text-center mt-3">Encontre a melhor viagem com assistência de IA</Text>
-        <TouchableOpacity onPress={() => router.push("/sign-in")} className="bg-orange-500 rounded-3xl p-5 mt-5 w-full">
-          <Text className="text-center text-white">Login</Text>
+        <TouchableOpacity
+          onPress={() => router.push('/(tabs)/home')}
+          className="bg-green-50 rounded-3xl p-5 mt-5 w-full"
+        >
+          <Text className="text-center text-white">Começar</Text>
         </TouchableOpacity>
       </View>
     </View>
